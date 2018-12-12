63e2ed30d38e3b4c8916132e99820e713ba9f09c
Guillermo Pineda <pinedag395@gmail.com>
2018-12-11 18:20:21 -0800

    Created the start page

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+function draw() {
+  const canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillRect(0, 0, 600, 900);
+    ctx.font = '48px serif';
+    ctx.fillStyle = 'white';
+    ctx.fillText('Press the space bar to begin', 25, 450);
+  }
+}
